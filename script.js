3b2871a5a342b7362d339cb60f001a17edc2d61a
Aman Mittal <mittalaman797@gmail.com>
2025-10-08 12:48:49 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 //your JS code here. If required.
+ function removeSelectedColor() {
+      const select = document.getElementById("colorSelect");
+      const selectedIndex = select.selectedIndex;
+
+      // Only remove if something is selected
+      if (selectedIndex !== -1) {
+        select.remove(selectedIndex);
+      }
+    }
